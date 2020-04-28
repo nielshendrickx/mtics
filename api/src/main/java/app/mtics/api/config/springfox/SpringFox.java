d4adf22c1dc6d455e681f7bbcc22b920f28334a5
unknown <niels.hendrickx@hotmail.com>
2020-04-28 19:19:43 +0200

    Skeleton

---
+++ api/src/main/java/app/mtics/api/config/springfox/SpringFox.java
@@ -0,0 +1,38 @@
+package app.mtics.api.config.springfox;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+
+@Configuration
+@EnableSwagger2
+public class SpringFox {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("app.mtics.api.endpoints"))
+                .build()
+                .apiInfo(apiDetails());
+    }
+
+    private ApiInfo apiDetails() {
+        return new ApiInfo(
+                "MTICS API",
+                "TODO",
+                "1.0",
+                "TODO",
+                new springfox.documentation.service.Contact("Niels Hendrickx", "TODO", "TODO"),
+                "API License",
+                "TODO",
+                Collections.emptyList()
+        );
+    }
+}
---
+++ api/src/main/java/app/mtics/api/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,11 @@
+package app.mtics.api.exceptions;
+
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
